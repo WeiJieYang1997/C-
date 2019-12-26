c74d69c468e85a41e2775fba8bd21c7983de74d8
WeiJIe Yang <38777825+WeiJieYang1997@users.noreply.github.com>
2019-12-25 20:03:43 -0500

    Add files via upload

---
+++ TaskPaneHostUI.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace _1225
+{
+    [ProgId(TaskPlane.SwTaskPane_ProgId)]
+    public partial class TaskPaneHostUI : UserControl
+    {
+        public TaskPaneHostUI()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TaskPlane.cs
@@ -0,0 +1,47 @@
+﻿using SolidWorks.Interop.sldworks;
+using SolidWorks.Interop.swpublished;
+
+namespace _1225
+{
+    /// <summary>
+    /// Our Solidworks taskpane add-in
+    /// </summary>
+    public class TaskPlane : ISwAddin
+    {
+        #region Private Members
+
+        /// <summary>
+        /// The cookie to the current instant of solidwork
+        /// </summary>
+        private int mSwCookie;
+
+        /// <summary>
+        /// The taskpane view for add-in
+        /// </summary>
+        private TaskpaneView mTaskpaneView;
+
+        /// <summary>
+        /// The UI control that is going to be inside the solidwork taskpane view
+        /// </summary>
+        private TaskPaneHostUI mTaskpaneHost;
+
+        /// <summary>
+        /// The current instance of solidworks application
+        /// </summary>
+        private SldWorks mSolidworksApplication;
+
+
+
+
+        #endregion
+
+        #region Public Members
+        
+        /// <summary>
+        /// The unique Id to the taskpane used for registration in COM
+        /// </summary>
+
+        public const string SwTaskPane_ProgId = "JustTaskPane";
+        #endregion
+    }
+}
